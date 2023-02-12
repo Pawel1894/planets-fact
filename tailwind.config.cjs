ad8c84ba1857c8d3ed68e4dd49efadc022a0cfb6
Pawel1894 <pawepohl@gmail.com>
2023-02-12 18:47:15 +0100

    Tailwind config, adding fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,28 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Spartan", "sans-serif"],
+      antonio: ["Antonio", "sans-serif"],
+    },
+    colors: {
+      white: "#ffffff",
+      neutral: {
+        100: "#070724",
+        200: "#38384F",
+        300: "#838391",
+      },
+      accent: {
+        100: "#419EBB",
+        200: "#EDA249",
+        300: "#6f2ed6",
+        400: "#D14C32",
+        500: "#D83A34",
+        600: "#CD5120",
+        700: "#1ec2a4",
+        800: "#2d68f0",
+      },
+    },
     extend: {},
   },
   plugins: [],
