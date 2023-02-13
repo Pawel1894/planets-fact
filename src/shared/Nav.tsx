4b0918d1c069458c780d64910863afd874db91b2
Pawel1894 <pawepohl@gmail.com>
2023-02-13 23:03:15 +0100

    Desktop header

--- src/shared/Nav.tsx
+++ src/shared/Nav.tsx
@@ -11,17 +11,17 @@ export default function Nav({ isOpen }: Props) {
       className={`${
         !isOpen && window.innerWidth < 768 ? "hidden" : "flex"
       }  absolute w-full left-0 top-[75px] h-[calc(100vh-75px)] bg-neutral-100 z-10
-      md:static md:h-auto`}
+      md:static md:h-auto xl:w-auto xl:ml-auto `}
     >
-      <ul className="w-full mt-8 px-6 md:flex md:gap-x-8 justify-center md:px-0">
-        <NavItem name="mercury" color="bg-accent-100" key={"mercury"} />
-        <NavItem name="venus" color="bg-accent-200" key={"venus"} borderTop={true} />
-        <NavItem name="earth" color="bg-accent-300" key={"earth"} borderTop={true} />
-        <NavItem name="mars" color="bg-accent-400" key={"mars"} borderTop={true} />
-        <NavItem name="jupiter" color="bg-accent-500" key={"jupiter"} borderTop={true} />
-        <NavItem name="saturn" color="bg-accent-600" key={"saturn"} borderTop={true} />
-        <NavItem name="uranus" color="bg-accent-700" key={"uranus"} borderTop={true} />
-        <NavItem name="neptune" color="bg-accent-800" key={"neptune"} borderTop={true} />
+      <ul className="w-full mt-8 px-6 md:flex md:gap-x-8 justify-center md:px-0 xl:justify-end xl:mt-0">
+        <NavItem name="mercury" color="accent-100" key={"mercury"} />
+        <NavItem name="venus" color="accent-200" key={"venus"} borderTop={true} />
+        <NavItem name="earth" color="accent-300" key={"earth"} borderTop={true} />
+        <NavItem name="mars" color="accent-400" key={"mars"} borderTop={true} />
+        <NavItem name="jupiter" color="accent-500" key={"jupiter"} borderTop={true} />
+        <NavItem name="saturn" color="accent-600" key={"saturn"} borderTop={true} />
+        <NavItem name="uranus" color="accent-700" key={"uranus"} borderTop={true} />
+        <NavItem name="neptune" color="accent-800" key={"neptune"} borderTop={true} />
       </ul>
     </nav>
   );
--- src/shared/NavItem.tsx
+++ src/shared/NavItem.tsx
@@ -3,8 +3,38 @@ import IconChevron from "../assets/icon-chevron.svg";
 
 type Props = {
   name: string;
-  color: string;
+  color:
+    | "accent-100"
+    | "accent-200"
+    | "accent-300"
+    | "accent-400"
+    | "accent-500"
+    | "accent-600"
+    | "accent-700"
+    | "accent-800";
   borderTop?: boolean;
+};
+
+const hoverVariants = {
+  "accent-100": "xl:hover:border-t-accent-100",
+  "accent-200": "xl:hover:border-t-accent-200",
+  "accent-300": "xl:hover:border-t-accent-300",
+  "accent-400": "xl:hover:border-t-accent-400",
+  "accent-500": "xl:hover:border-t-accent-500",
+  "accent-600": "xl:hover:border-t-accent-600",
+  "accent-700": "xl:hover:border-t-accent-700",
+  "accent-800": "xl:hover:border-t-accent-800",
+};
+
+const bgVariants = {
+  "accent-100": "bg-accent-100",
+  "accent-200": "bg-accent-200",
+  "accent-300": "bg-accent-300",
+  "accent-400": "bg-accent-400",
+  "accent-500": "bg-accent-500",
+  "accent-600": "bg-accent-600",
+  "accent-700": "bg-accent-700",
+  "accent-800": "bg-accent-800",
 };
 
 export default function NavItem({ name, color, borderTop }: Props) {
@@ -12,11 +42,11 @@ export default function NavItem({ name, color, borderTop }: Props) {
     <li
       className={`flex py-5 uppercase text-white font-bold md:py-0 ${
         borderTop && "border-t border-t-neutral-200"
-      } md:border-t-0`}
+      } md:border-t-0 xl:border-t-4 xl:border-t-[transparent] ${hoverVariants[color]} xl:py-6`}
     >
       <button className="flex items-center justify-between w-full">
         <div className="flex items-center gap-6">
-          <span className={`md:hidden h-5 w-5 rounded-full ${color}`} aria-hidden={true}></span>
+          <span className={`md:hidden h-5 w-5 rounded-full ${bgVariants[color]}`} aria-hidden={true}></span>
           <span className="uppercase">{name}</span>
         </div>
         <img className="md:hidden" src={IconChevron} aria-hidden={true} alt="" />
