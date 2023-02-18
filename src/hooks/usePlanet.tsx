7b8910baa0421ae56dee211b6df66f0e24918cc2
Pawel1894 <pawepohl@gmail.com>
2023-02-18 13:44:08 +0100

    removing fake delay

--- src/hooks/usePlanet.tsx
+++ src/hooks/usePlanet.tsx
@@ -3,7 +3,6 @@ import { TPlanet } from "../types";
 
 export default function usePlanet(planetName?: string) {
   const [planet, setPlanet] = useState<TPlanet>();
-  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<any>();
 
   useEffect(() => {
@@ -11,8 +10,6 @@ export default function usePlanet(planetName?: string) {
   }, [planetName]);
 
   async function fetchPlanet() {
-    setIsLoading(true);
-
     try {
       const response = await fetch("/data.json");
       if (!response.ok || response.status !== 200) throw new Error("error fetching data");
@@ -25,14 +22,10 @@ export default function usePlanet(planetName?: string) {
     }
 
     // mimic real request delay..
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 700);
   }
 
   return {
     planet,
-    isLoading,
     error,
   };
 }
